ea2beda20f96c13adde143c088907f004cfa868a
aidant <aidan.temple1@gmail.com>
2018-07-03 22:54:34 +1000

    update run tests

--- src/shell/run.test.ts
+++ src/shell/run.test.ts
@@ -2,20 +2,38 @@ import { expect } from 'chai'
 import 'mocha'
 
 import { run } from './run'
+import { Stream, Readable } from 'stream';
+import { streamToBuffer } from './stream-to-buffer';
+
+const echo = 'Hello World!!'
+let stdout: Stream
+let stderr: Stream
+let exit: Promise<number>
 
 describe('run', () => {
-  const echo = 'Hello World!!'
-  it('should resolve with strings', async () => {
-    const { stdout, stderr, code } = await run(`echo "${echo}"`)
-    expect(stdout).to.equal(echo)
-    expect(stderr).to.equal('')
+  it(`returns two streams and a promise`, () => {
+    const result = run(`echo "${echo}"`)
+    stdout = result.stdout
+    stderr = result.stderr
+    exit = result.exit
+
+    expect(stdout instanceof Stream).true
+    expect(stderr instanceof Stream).true
+    expect(exit instanceof Promise).true
+  })
+
+  it(`should exit with code 0`, async () => {
+    const code = await exit
     expect(code).to.equal(0)
   })
 
-  it('should resolve with buffers', async () => {
-    const { stdout, stderr, code } = await run(`echo "${echo}"`, { buffer: false })
-    expect(stdout).to.equal(Buffer.from(echo, 'utf8'))
-    expect(stderr).to.equal(Buffer.from('', 'utf8'))
-    expect(code).to.equal(0)
+  it(`stdout matches ${echo}`, async () => {
+    const result = await streamToBuffer(stdout)
+    expect(result.toString()).to.match(new RegExp(echo))
+  })
+
+  it(`stderr should be empty`, async () => {
+    const result = await streamToBuffer(stderr)
+    expect(result.toString()).to.match(/(?!.+)/)
   })
 })
